cdfc2dcfe6d153bf997cedeba0a67ce2fffe4567
kishore-shiva <kishoreshiva.s@gmail.com>
2021-10-02 10:42:31 +0530

    synchronous character writing completed

--- typeAutomation.py
+++ typeAutomation.py
@@ -10,10 +10,11 @@ y = int(input('enter y position: '))
 
 f = open('enterTheText.txt', 'r')
 lines = f.readlines()
+text = ''
+for line in lines:
+    text += line
 
 df.moveTo(x, y, duration=2)
 df.click(x, y)
-for line in lines:
-    df.typewrite(line)
-    time.sleep(0.5)
-    df.typewrite(['enter'])
+for character in text:
+    df.write(character)
